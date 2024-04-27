587e7cab3002ae40b7f13b41446161dff4b8eb9b
masud4657 <masudpfrnt@gmail.com>
2024-04-27 15:07:10 +0600

    added the users information in the database.

--- index.js
+++ index.js
@@ -29,15 +29,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const productCollection = client.db("productDB").collection("product")
+    const userCollection = client.db("userDB").collection("user")
+
+    app.post('/user',async(req,res)=>{
+      const user = req.body
+      console.log(user)
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/user', async(req,res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
 
 app.get('/', (req, res) => {
