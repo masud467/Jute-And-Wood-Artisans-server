42db028ab6d1f802f4c1750f1606210f26589077
masud4657 <masudpfrnt@gmail.com>
2024-04-29 09:16:30 +0600

    added the specific Product for specific user in the mongodb.

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/myProduct/:email',async(req,res)=>{
+      console.log(req.params.email)
+      const result = await productCollection.find({email:req.params.email}).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
